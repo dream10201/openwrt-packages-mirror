2af27df07cf942614c89af5755f3e6738d47e423
dream10201 <xiuxiu10201@gmail.com>
2021-03-10 16:14:08 +0800

    Update openwrt_mirror.py

--- openwrt_mirror.py
+++ openwrt_mirror.py
@@ -10,34 +10,16 @@ proxies = {"http": "http://127.0.0.1:10808","https": "http://127.0.0.1:10808"}
 import requests
 import re
 import os
-from concurrent.futures import ThreadPoolExecutor,ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
 import threading
-import ssl
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.poolmanager import PoolManager
-class SSLAdapter(HTTPAdapter):
-    '''An HTTPS Transport Adapter that uses an arbitrary SSL version.'''
-    def __init__(self, ssl_version=None, **kwargs):
-        self.ssl_version = ssl_version
-
-        super(SSLAdapter, self).__init__(**kwargs)
-
-    def init_poolmanager(self, connections, maxsize, block=False):
-        self.poolmanager = PoolManager(num_pools=connections,
-                                       maxsize=maxsize,
-                                       block=block,
-                                       ssl_version=self.ssl_version)
-
 
 chunk_size=512*4
 requests.adapters.DEFAULT_RETRIES = 5
 request = requests.Session()
-#request.mount('https://', SSLAdapter(ssl.PROTOCOL_TLSv1))
 #request.proxies = proxies
 request.headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36"}
 request.keep_alive = False
 threadPool = ThreadPoolExecutor(max_workers=12, thread_name_prefix="download_")
-#processPool = ProcessPoolExecutor()
 lock=threading.Lock()
 count = 0
 fail_list = []
@@ -89,13 +71,11 @@ def save_packages(url, location):
             item = item.replace('%2b', '+')
             if not os.path.isfile(location + item):
                 threadPool.submit(download, location + item,url + item,item)
-                #processPool.submit(download, location + item,url + item,item)
                 count +=1
 
 if __name__ == '__main__':
     for url in packages_url:
         save_packages(url, save_path+url.replace("https://downloads.openwrt.org",""))
     threadPool.shutdown(wait=True)
-    #processPool.shutdown(wait=True)
     failStr = "\n".join(fail_list)
     print(f"Total {count}\ndone.\n\033[1;31;40m{failStr}\033[0m")
